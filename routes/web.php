08593712d052f962e51107926463b1405daf0e09
zayyanerdiansyah <zayyanerdiansyah@gmail.com>
2021-04-25 05:51:20 +0700

    Display All Questions

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QuestionsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('questions',QuestionsController::class);
 
 Auth::routes();
 
