9ad4d42d891884d3a59900122c5667c006f0f3c8
zayyanerdiansyah <zayyanerdiansyah@gmail.com>
2021-05-05 08:50:32 +0700

    deleting answer include best answer id on question in database layer

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -40,4 +40,8 @@ class Answer extends Model
             $answer->question->decrement('answers_count');
         });
     }
+
+    public function getStatusAttribute(){
+        return $this->id == $this->question->best_answer_id ? 'vote-accepted' : '' ;
+    }
 }
