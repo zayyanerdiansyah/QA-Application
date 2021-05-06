0a6e42c59ce3d10515bec6bf4546446c35b69188
zayyanerdiansyah <zayyanerdiansyah@gmail.com>
2021-05-06 13:56:27 +0700

    Refactoring views for voting and author

---
+++ resources/views/shared/_author.blade.php
@@ -0,0 +1,12 @@
+<span class="text-muted">
+  {{ $label }} {{$model->created_date}}
+</span>
+<div class="media mt-2">
+  <a href="{{$model->user_url}}" class="pr-2">
+    <img src="{{$model->user->avatar}}">
+  </a>
+  <div class="media-body mt-1">
+    <a href="{{$model->user->url}}">{{$model->user->name}}
+    </a>
+  </div>
+</div>
